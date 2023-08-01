950e2b0560f18e2c7cd2f2c56002c17f79bab40f
German Encina <gencina2@gmail.com>
2023-07-31 21:50:39 -0400

    Ahora si Hito 1

--- app/src/main/java/cl/gencina/roommvvm2/data/local/ItemEntity.kt
+++ app/src/main/java/cl/gencina/roommvvm2/data/local/ItemEntity.kt
@@ -4,6 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "item_table")
-class ItemEntity (val name:String, val price: Int, val quantity: Int){
+data class ItemEntity (val name:String, val price: Int, val quantity: Int){
     @PrimaryKey(autoGenerate = true) var id: Long = 0
 }
